accbd600b854684a45d3f6edca9c24751b1187be
Wykthor <wykthor.g@wgvale.com>
2017-04-28 19:53:27 -0300

    Inicio de trabalho

--- Tabuleiro.c
+++ Tabuleiro.c
@@ -1,7 +1,72 @@
 #include <stdio.h>
-#DEFINE DEBUG if(0)
+#include <stdlib.h>
+#include "Tabuleiro.h"
+#define DEBUG if(0)
 
-int main(int argc, char **argv)
+refTabuleiro init(int *pos, int *dim, char *bitmap, char *nome)
 {
-	return 0;
+	refTabuleiro novo = malloc(sizeof(tabuleiro));
+	novo->posicao = malloc(sizeof(int)*2);
+	novo->posicao = pos;
+	novo->dimensao = malloc(sizeof(int)*2);
+	novo->dimensao = dim;
+	novo->bitmap = malloc(sizeof(char));
+	strcpy(novo->bitmap,bitmap);
+	novo->nome = malloc(sizeof(char)*10);
+	strcpy(novo->nome,nome);
+	return novo;
 }
+
+void fechar(refTabuleiro morto)
+{
+	free(morto->posicao);
+	free(morto->dimensao);
+	free(morto->bitmap);
+	free(morto->nome);
+	morto->posicao = 	NULL;
+	morto->dimensao = 	NULL;
+	morto->bitmap = 	NULL;
+	morto->nome = 		NULL;
+	free(morto);
+	morto = NULL;
+}
+
+int *pegarPos(refTabuleiro atual)
+{
+	return atual->posicao;
+}
+
+int *pegarDimensao(refTabuleiro atual)
+{
+	return atual->dimensao;
+}
+
+char *pegarBitmap(refTabuleiro atual)
+{
+	return atual->bitmap;
+}
+char *pegarNome(refTabuleiro atual)
+{
+	return atual->nome;
+}
+
+void mudarPos(int *novaPos,refTabuleiro atual)
+{
+	atual->pos = novaPos;
+}
+
+void mudarDimensao(int *novaDimensao,refTabuleiro atual)
+{
+	atual->dimensao = novaDimensao;
+}
+
+void mudarBitmap(char *novoBitmap,refTabuleiro atual)
+{
+	strcpy(atual->bitmap,novoBitmap);
+}
+
+void mudarNome(char *novoNome,refTabuleiro atual)
+{
+	strcpy(atual->nome,novoNome);
+}
+
--- Tabuleiro.h
+++ Tabuleiro.h
@@ -1,7 +1,40 @@
 #include <stdio.h>
-#DEFINE DEBUG if(0)
+#include <string.h>
 
-int main(int argc, char **argv)
+typedef struct tabuleiro tabuleiro;
+typedef struct tabuleiro *refTabuleiro;
+
+struct tabuleiro
 {
-	return 0;
-}
+	int *posicao;
+	int *dimensao;
+	//insira ponteiro definido para pecas aqui
+	char *bitmap;
+	//char **sons;
+	char *nome;
+};
+
+refTabuleiro init(int *pos, int *dim, char *bitmap, char *nome);
+//Inicializa o objeto referenciado pelo ponteiro refTabuleiro.
+
+void fechar(refTabuleiro morto);
+//Destroi todas as referencias e libera as areas de memoria apropriadas.
+
+int *pegarPos(refTabuleiro atual);
+
+int *pegarDimensao(refTabuleiro atual);
+
+char *pegarBitmap(refTabuleiro atual);
+
+char *pegarNome(refTabuleiro atual);
+
+void mudarPos(int *novaPos,refTabuleiro atual);
+
+void mudarDimensao(int *novaDimensao,refTabuleiro atual);
+
+void mudarBitmap(char *novoBitmap,refTabuleiro atual);
+
+void mudarNome(char *novoNome,refTabuleiro atual);
+
+
+	
