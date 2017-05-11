12c5b0f2f147afbe91395ba3ecb68b901bb21a63
arquimedes vinicius <arquimedes vinicius>
2017-05-10 23:12:10 -0300

    Update

--- allegro inicialização.c
+++ allegro inicialização.c
@@ -11,11 +11,11 @@ int main()
 	set_gfx_mode(GFX_AUTODETECT_WINDOWED, 800, 600, 0, 0);
 	set_window_title("Hello World");
 
-	while(!KEY[KEY_ESC])
+	while(!key[KEY_ESC])
 	{
-		textout_centre_ex(screen, font, "Hello World", SCREEN_W/2, makecol(255, 255, 255), -1);
+		textout_centre_ex(screen, font, "Hello World", SCREEN_W/2,SCREEN_H/2, makecol(255, 255, 255), -1);
 	}
-	
+
 	return 0;
 }
 END_OF_MAIN();
